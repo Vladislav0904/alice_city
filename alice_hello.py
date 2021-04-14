b31def229b2f0f57ce75afb6d76e406cbc7613fb
Vladislav <vladislav0904@gmail.com>
2021-04-14 17:05:02 +0500

    heroku 2
    Signed-off-by: Vladislav <vladislav0904@gmail.com>

--- alice_hello.py
+++ alice_hello.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import logging
 import json
 import random
+import os
 
 app = Flask(__name__)
 
@@ -120,4 +121,6 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
